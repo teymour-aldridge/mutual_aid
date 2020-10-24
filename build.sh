694a671242ea1cc6e21899c5a155dac8295d9b95
teymour-aldridge <teymour.aldridge@icloud.com>
2020-10-24 20:06:41 +0100

    Actually install all the things

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 cd client;
-npm install react-scripts
+npm install
 npm run-script build;
 cd ../
 mv client/build server
 cd server
 npm install --global rollup
+npm install
 rollup --config rollup.config.js
 cat bundle.js
 cd ../
